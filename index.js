bdaf8527f1017732ba00b9bb21db3debacd4cdca
aayush <aayushshah128@gmail.com>
2021-11-24 09:43:05 +0530

    db error

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }))
 
-mongoose.connect('mongodb+srv://aayush:<aayush>@cluster0.ndxjj.mongodb.net/mydb',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect('mongod://localhost:27017/mydb',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 var db = mongoose.connection;
